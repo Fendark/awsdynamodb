2692fb48a6df88a18ed29e31c56b6d923257715a
Gérôme Cabezas <Grme Cabezas>
2017-05-16 16:01:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express"), http = require("http");
+app = express();
+
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+	region : process.env.DYNAMO_REGION,
+	endpoint : process.env.DYNAMO_ENDPOINT,
+});
+
+app.get('/', function(req,res){
+
+});
+
+app.get('/users', function(req,res){
+
+});
+
+app.get('/user/:userid', function(req,res){
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	var params = {
+		TableName:"users",
+		Key:{
+			"userid":"",
+			"promo":""
+		}
+	};
+
+	docClient.get(params,function(err,data){
+		if(err){
+			res.send("Unable to read item. Error JSON :"+JSON.stringify(err,null,2));
+		}else{
+			res.send("Get Item succeeded : "+ JSON.stringify(data, null,2));
+		}
+	});
+
+});
+
+app.listen(process.env.PORT, function(){
+	console.log("OK");
+});
+
